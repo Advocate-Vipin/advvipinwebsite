00abf781303800f06cf1937d2e4671a0b48b1513
MEHUL GARG <mehul.garg104@gmail.com>
2025-06-30 00:03:08 +0530

    navigation issue- Not navigating, showing 404 Not found Error- ISSUE Resolved

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -106,8 +106,8 @@ export default function Navbar() {
             <DisclosureButton
               key={item.name}
               as="a"
-              href={item.href}
-              aria-current={item.current ? 'page' : undefined}
+              onClick={navigationClicks(item.href)}
+              style={{ cursor: 'pointer' }}
               className={classNames(
                 item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                 'block rounded-md px-3 py-2 text-base font-medium',
